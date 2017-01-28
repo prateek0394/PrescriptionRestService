2a72a64163b34c28ae7a61f28d585e7dd6a0e5f8
Prateek <jainpj@amazon.com>
2017-01-28 17:39:18 +0530

    Project initialization

---
+++ src/main/java/com/sniper/PrescriptionRestServiceApplication.java
@@ -0,0 +1,12 @@
+package com.sniper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrescriptionRestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrescriptionRestServiceApplication.class, args);
+	}
+}
